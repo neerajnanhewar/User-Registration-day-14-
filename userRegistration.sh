9ca24041049513a1b1834ba5bc5596c0fa05bd0b
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-07-24 16:59:55 +0530

    Refactore : Refactore the code and LastName rule Added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,11 +1,29 @@
 #!/bin/bash -x
-echo "Name should starts with capital"
-echo "Enter Your First Name " 
-read firstname
+
+function validate()
+{
+input=$1 
+key=0
 pattern="^[A-Z]{1}[a-z]{2}"
-if [[ $firstname =~ $pattern ]]
+while [ $key -eq 0 ]
+do
+if [[ $input =~ $pattern ]]
 then
-	echo $firstname "is valid userName"
+	echo $input "is valid INPUT"
+	key=1
 else
-	echo "Not valid username"
+	echo "Not valid INPUT"
+	read $input
 fi
+done
+}
+
+echo "FirstName should starts with capital letter"
+echo "Enter Your First Name "
+read firstname
+validate $firstname
+
+echo "LastName should starts with capital letter"
+echo "Enter Your Last Name "
+read lastname
+validate $lastname
