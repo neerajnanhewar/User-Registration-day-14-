f79f52efebcba8d2898ced266432bc0d8683c9e1
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-07-25 15:32:31 +0530

    Refactore: refactore the initial code

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,7 +2,8 @@
 
 function user_validate()
 {
-input=$1 
+input=$1
+pattern=$2 
 key=0
 pattern="^([A-Z]{1})[a-z]{2}"
 while [ $key -eq 0 ]
@@ -18,36 +19,20 @@ fi
 done
 }
 
-function email_validate()
-{
-input=$1
-key=0
-email_ptrn="^[a-zA-Z0-9]{1,}([._+-][0-9a-zA-Z]+)*[@]{1}[0-9a-zA-Z]{1,}\.[a-zA-Z]{2,4}([.][a-zA-Z]{2}){0,1}$"
-while [ $key -eq 0 ]
-do
-if [[ $input =~ $email_ptrn ]]
-then
-   echo $input "is valid INPUT"
-   key=1
-else
-   echo "Not valid INPUT"
-   read input
-fi
-done
-
-
-}
+pattern="^([A-Z]{1})[a-z]{2}"
 
 echo "FirstName should starts with capital letter"
 echo "Enter Your First Name "
 read firstname
-user_validate $firstname
+user_validate $firstname $pattern
 
 echo "LastName should starts with capital letter"
 echo "Enter Your Last Name "
 read lastname
-user_validate $lastname
+user_validate $lastname $pattern
 
 echo "Enter Your Last Email "
 read email
-email_validate $email
+pattern="^[a-zA-Z0-9]{1,}([._+-][0-9a-zA-Z]+)*[@]{1}[0-9a-zA-Z]{1,}\.[a-zA-Z]{2,4}([.][a-zA-Z]{2}){0,1}$"
+user_validate $email $pattern
+
