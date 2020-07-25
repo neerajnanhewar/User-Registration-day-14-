268becee6780b8d265da5f9cba867e28b4dc7919
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-07-25 15:21:27 +0530

    Added: user Email validation rule added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 
-function validate()
+function user_validate()
 {
 input=$1 
 key=0
-pattern="^[A-Z]{1}[a-z]{2}"
+pattern="^([A-Z]{1})[a-z]{2}"
 while [ $key -eq 0 ]
 do
 if [[ $input =~ $pattern ]]
@@ -13,17 +13,41 @@ then
 	key=1
 else
 	echo "Not valid INPUT"
-	read $input
+	read input
 fi
 done
+}
+
+function email_validate()
+{
+input=$1
+key=0
+email_ptrn="^[a-zA-Z0-9]{1,}([._+-][0-9a-zA-Z]+)*[@]{1}[0-9a-zA-Z]{1,}\.[a-zA-Z]{2,4}([.][a-zA-Z]{2}){0,1}$"
+while [ $key -eq 0 ]
+do
+if [[ $input =~ $email_ptrn ]]
+then
+   echo $input "is valid INPUT"
+   key=1
+else
+   echo "Not valid INPUT"
+   read input
+fi
+done
+
+
 }
 
 echo "FirstName should starts with capital letter"
 echo "Enter Your First Name "
 read firstname
-validate $firstname
+user_validate $firstname
 
 echo "LastName should starts with capital letter"
 echo "Enter Your Last Name "
 read lastname
-validate $lastname
+user_validate $lastname
+
+echo "Enter Your Last Email "
+read email
+email_validate $email
